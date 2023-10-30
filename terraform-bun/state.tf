1670852b11afab1c301a01016110f92a1e8f113c
flyck <felix.brilej@googlemail.com>
2023-10-30 17:34:20 +0100

    fix(terraform-bun): minor updates

--- terraform-bun/state.tf
+++ terraform-bun/state.tf
@@ -3,17 +3,16 @@ provider "aws" {
 }
 
 # for bootstrap comment out this block first and then run a `terraform init -migrate-state`. This
-# will give you the production-like experience and realisitc deployment times with a shared state
+# will give you the production-like experience and realistic deployment times with a shared state
 # instead of a local state
 terraform {
   backend "s3" {
     bucket         = "terraform-bun-state"
-    key            = "test/terraform-bun-state.tfstate"
+    key            = "terraform-bun-state.tfstate"
     region         = "eu-central-1"
     dynamodb_table = "terraform-bun-state"
   }
 }
-
 
 resource "aws_s3_bucket" "state_bucket" {
   bucket = "terraform-bun-state"
